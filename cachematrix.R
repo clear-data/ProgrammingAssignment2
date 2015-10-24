83a87ebe1af408f49fbce9d4400b75c209d82142
clear-data <alexanderszewczak@mac.com>
2015-10-24 00:15:06 -0400

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCasheMatrix and cacheSolve
+## Functions do the following ...
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverseV <- NULL
+        set <- function(y) {
+                x <<- y
+                inverseV <<- NULL
+        }
+        get <-function() x
+        setinverse <- function(inverseF) inverseS <<- inverseF
+        getinverse <- function() inverseV
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverseV <- x$getinverse()
+        if(!is.null(inverseV)) {
+                message("getting cached data")
+                return(inverseV)
+        }
+        data <- x$get()
+        inverseV <--inverseF(data, ...)
+        x$setinverse(inverseV)
+        inverseV
 }
